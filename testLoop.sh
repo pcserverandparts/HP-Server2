869ee90b711581b292d3aae1e413a46b2ce63efc
pcserverandparts <pcserverandparts.shared@gmail.com>
2022-05-23 13:28:48 -0400

    Tested on May 23 01:28:48 PM

---
+++ testLoop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+count=1
+
+while true; do
+	echo -e "\e[12A\e[KUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM \
+		\nUpdating $RANDOM"
+
+	sleep 1;
+done
